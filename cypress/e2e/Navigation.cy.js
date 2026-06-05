0b823e7190ace2c04ff9789606d03716fe7b521a
Yael <yaelsilvestre64@gmail.com>
2026-06-05 09:59:46 -0600

    Navegation test finished

--- cypress/e2e/Navigation.cy.js
+++ cypress/e2e/Navigation.cy.js
@@ -1,15 +1,43 @@
 /// <reference types="cypress" />
+describe('Navigation Test', () => {
 
-describe('first', () => {
-    it("NavigationTest",()=>{
-        cy.visit("https://opencart.abstracta.us/");
-        cy.title().should('eq','Your Store'); //Home Page
+    it('NavigationTest', () => {
 
-        cy.get("//a[normalize-space()='Cameras']").click();
-        cy.get("div[id='content'] h2").should('have.text','Cameras'); //Cameras
+        cy.visit('https://opencart.abstracta.us/');
 
-        cy.go('back'); //Go back to home page
-        cy.title().should('eq','Your Store'); //Home Page
+        // Home
+        cy.title().should('eq', 'Your Store');
 
-    })
-})
+        // Click en Cameras
+        cy.get('#menu a[href*="path=33"]')
+          .should('be.visible')
+          .click();
+
+        // Validar URL
+        cy.url().should('include', 'path=33');
+
+        // Validar página Cameras
+        cy.get('h2').should('contain', 'Cameras');
+
+        // Regresar
+        cy.go('back');
+        cy.title().should('eq', 'Your Store');
+
+        // Avanzar
+        cy.go('forward');
+        cy.url().should('include', 'path=33');
+
+        // Atrás
+        cy.go(-1);
+        cy.title().should('eq', 'Your Store');
+
+        // Adelante
+        cy.go(1);
+        cy.url().should('include', 'path=33');
+
+        // Recargar
+        cy.reload();
+
+    });
+
+});
